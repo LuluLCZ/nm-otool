b5db3cfaff6cfcdf76bb5cd7d05f5687b0e2a571
Lucien LACAZE <llacaze@e3r9p15.42.fr>
2019-11-19 18:18:19 +0100

    goodnight

--- nm/sources/main.c
+++ nm/sources/main.c
@@ -6,25 +6,71 @@
 /*   By: llacaze <llacaze@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/18 16:51:32 by llacaze           #+#    #+#             */
-/*   Updated: 2019/11/18 17:37:41 by llacaze          ###   ########.fr       */
+/*   Updated: 2019/11/19 17:41:18 by llacaze          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/ft_nm.h"
+#include "../libft/includes/libft.h"
+
+void	ft_putnbr_base(size_t n, int base)
+{
+	if (base == 10)
+		ft_putnbr(n);
+	else if (n < 0)
+		ft_putchar('0');
+	else
+	{
+		if (n >= base)
+			ft_putnbr_base(n / base, base);
+		if (n % base < 10)
+			ft_putchar((n % base) + 48);
+		else
+			ft_putchar((n % base) - 10 + 65);
+	}
+}
 
 void print_output(int nsyms, int symoff, int stroff, char *ptr)
 {
 	int i;
 	char *stringtable;
-	struct nlist_64 *el;
+	struct nlist_64 *element;
+	struct nlist_64 *testu;
+	const char *type;
 
 	i = 0;
-	el = (void *)ptr + symoff;
+	int j = 0;
+	element = (void *)ptr + symoff;
 	stringtable = (void *)ptr + stroff;
 	while (i < nsyms)
 	{
-		ft_putstr(stringtable + el[i].n_un.n_strx);
-		ft_putchar('\n');
+		type = NULL;
+		switch(element[i].n_type & N_TYPE) {
+			case N_UNDF: type = "U"; break;
+			case N_INDR:  type = "I"; break;
+			case N_ABS: type = "A"; break;
+			case N_SECT: type = "S"; break;
+			case N_PBUD: type = "P"; break;
+			default:
+				// fprintf(stderr, "Invalid symbol type: 0x%x ", element[i].n_type);
+				j = -1;
+				// return ;
+			}
+			char test[10];
+			if (j == 0 && ft_strlen(stringtable + element[i].n_un.n_strx) > 0) {
+				// printf("|| %x element type N_TYPE for %s||", element[i].n_type, stringtable + element[i].n_un.n_strx);
+				// printf("%d\n", element[i].n_type & N_TYPE);
+				// printf("%d\n", element[i].n_type & N_EXT);
+				printf("%d", element[i].n_sect);
+				// ft_putstr(type);
+				// ft_putchar(' ');
+				// ft_putnbr_base(element[i].n_value, 16);
+				// ft_putchar(' ');
+				// if (ft_strlen(stringtable + element[i].n_un.n_strx) == 0) ft_putstr("element + tringtable is NULL");
+				// else ft_putstr(stringtable + element[i].n_un.n_strx);
+				// ft_putchar('\n');
+			}
+		j = 0;
 		i++;
 	}
 }
@@ -38,6 +84,7 @@ void handle_64(char *ptr)
 	struct symtab_command *sym;
 
 	header = (struct mach_header_64 *)ptr;
+	// printf("%u -> MAGIC\n%u -> FILETYPE\n%u -> FLAGS\n", header->magic, header->filetype, header->flags);
 	ncmds = header->ncmds;
 	i = 0;
 	lc = (void *)ptr + sizeof(*header);
@@ -61,11 +108,11 @@ void nm(char *ptr)
 	magic_number = *(int *) ptr;
 	if (magic_number == MH_MAGIC)
 	{
-		puts("Binaire pour 32-bits");
+		// puts("Binaire pour 32-bits");
 	}
 	if (magic_number == MH_MAGIC_64)
 	{
-		puts("Binaire pour 64-bits");
+		// puts("Binaire pour 64-bits");
 		handle_64(ptr);
 	}
 }
